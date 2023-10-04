b8aeb4046a89c28b3d228008463ef1fa54acf9f3
Quinn Weber <quinn@quinnweber.com>
2023-10-03 19:39:30 -0700

    add nvm exports

--- src/zshrc.sh
+++ src/zshrc.sh
@@ -18,3 +18,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 ## poetry
 export PATH="/Users/quinnweber/.local/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
