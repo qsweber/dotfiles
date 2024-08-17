9a40544d8c0d6d7fc6ebd8393cf3882064147854
Quinn Weber <quinn@quinnweber.com>
2024-08-17 14:39:04 -0700

    direnv

--- src/zshrc.sh
+++ src/zshrc.sh
@@ -10,7 +10,7 @@ source $ZSH/oh-my-zsh.sh
 
 ## pyenv
 export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
 ## homebrew
@@ -22,3 +22,5 @@ export PATH="/Users/quinnweber/.local/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+eval "$(direnv hook zsh)"
