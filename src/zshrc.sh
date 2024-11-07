303b170b5522cbf1ba492bd4b0e2934a1cd98beb
Quinn Weber <quinn@quinnweber.com>
2024-11-07 09:57:57 -0800

    add comment

--- src/zshrc.sh
+++ src/zshrc.sh
@@ -19,8 +19,10 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 ## poetry
 export PATH="/Users/quinnweber/.local/bin:$PATH"
 
+## nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+## direnv
 eval "$(direnv hook zsh)"
